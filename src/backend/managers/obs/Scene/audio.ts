2631b8a346fb96b60b3eead4f6ede653a8c3baf8
sshcrack <hendrik.lind777@gmail.com>
2022-04-23 02:19:17 +0200

    I mean it works but is pretty messy

--- src/backend/managers/obs/Scene/audio.ts
+++ src/backend/managers/obs/Scene/audio.ts
@@ -10,37 +10,32 @@ const log = MainLogger.get("Backend", "Manager", "OBS", "Scene", "Audio");
 export class AudioSceneManager {
 
     static async initializeAudioSources(scene: IScene) {
+        //TODO clean up this
+
         log.info("Setting up audio sources...")
         Global.setOutputSource(1, scene);
         const execa = await (await import("execa")).execa;
         const defaultAudioSources = await execa(MainGlobals.nativeMngExe, ["audio"])
         const { desktop, mic } = JSON.parse(defaultAudioSources.stdout) as AudioReturn;
-
+        log.info("Desktop id is", desktop, "Mic id is", mic)
 
         setSetting(SettingsCat.Output, 'Track1Name', 'Mixed: all sources');
         let currentTrack = 2;
 
-        this.getAudioDevices(byOS({ [OS.Windows]: 'wasapi_output_capture', [OS.Mac]: 'coreaudio_output_capture' }), 'desktop-audio')
-            .forEach((metadata: { device_id: string, name: string }) => {
-                if (metadata.device_id.toLowerCase() !== desktop) return;
-                const source = InputFactory.create(byOS({ [OS.Windows]: 'wasapi_output_capture', [OS.Mac]: 'coreaudio_output_capture' }), 'desktop-audio', { device_id: metadata.device_id });
-                log.log("Setting up audio device source", metadata.name);
-                setSetting(SettingsCat.Output, `Track${currentTrack}Name`, metadata.name);
-                source.audioMixers = 1 | (1 << currentTrack - 1); // Bit mask to output to only tracks 1 and current track
-                Global.setOutputSource(currentTrack, source);
-                currentTrack++;
-            });
+        const desktopSource = InputFactory.create(byOS({ [OS.Windows]: 'wasapi_output_capture', [OS.Mac]: 'coreaudio_output_capture' }), 'desktop-audio', { device_id: desktop });
 
-        this.getAudioDevices(byOS({ [OS.Windows]: 'wasapi_input_capture', [OS.Mac]: 'coreaudio_input_capture' }), 'mic-audio')
-            .forEach((metadata: { device_id: string, name: string }) => {
-                if (metadata.device_id.toLowerCase() !== mic) return;
-                const source = InputFactory.create(byOS({ [OS.Windows]: 'wasapi_input_capture', [OS.Mac]: 'coreaudio_input_capture' }), 'mic-audio', { device_id: metadata.device_id });
-                log.log("Setting up mic source", metadata.name);
-                setSetting(SettingsCat.Output, `Track${currentTrack}Name`, metadata.name);
-                source.audioMixers = 1 | (1 << currentTrack - 1); // Bit mask to output to only tracks 1 and current track
-                Global.setOutputSource(currentTrack, source);
-                currentTrack++;
-            });
+        setSetting(SettingsCat.Output, `Track${currentTrack}Name`, "Desktop audio");
+        desktopSource.audioMixers = 1 | (1 << currentTrack - 1); // Bit mask to output to only tracks 1 and current track
+        Global.setOutputSource(currentTrack, desktopSource);
+        currentTrack++;
+
+        const micSource = InputFactory.create(byOS({ [OS.Windows]: 'wasapi_input_capture', [OS.Mac]: 'coreaudio_input_capture' }), 'mic-audio', { device_id: mic });
+
+        setSetting(SettingsCat.Output, `Track${currentTrack}Name`, "Microphone source");
+
+        micSource.audioMixers = 1 | (1 << currentTrack - 1); // Bit mask to output to only tracks 1 and current track
+        Global.setOutputSource(currentTrack, micSource);
+        currentTrack++;
 
         setSetting(SettingsCat.Output, 'RecTracks', parseInt('1'.repeat(currentTrack - 1), 2)); // Bit mask of used tracks: 1111 to use first four (from available six)
 
